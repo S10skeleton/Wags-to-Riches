05e07e49ad8cd27b4b0dba17f02226922b889ff2
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2024-02-05 19:24:16 -0700

    Added info into user model to match adoption form inputs

--- models/User.js
+++ models/User.js
@@ -16,7 +16,19 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    full_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    location: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -28,12 +40,27 @@ User.init(
         isEmail: true,
       },
     },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [10],
+      },
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [8],
       },
+    },
+    work_schedule: {
+      type: DataTypes.ENUM('full time', 'part time', 'work from home'),
+      allowNull: false,
+    },
+    personal_statement: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
   },
   {

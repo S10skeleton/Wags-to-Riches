79b0ce45376bbfabaea9e4540a0dbaedf529e7c0
J Mac <jbatcheller123@gmail.com>
2024-02-07 10:37:39 -0700

    Added the correct route for the user login

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-
+const userRoutes = require('././api/userRoutes')
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
-
+router.use('/api', apiRoutes);
+router.use('/login', userRoutes);
 module.exports = router;

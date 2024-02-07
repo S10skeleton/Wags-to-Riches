1686faa6653ee0fb316d57d264b0c598751c84cd
Thomas Holtam <thomasjholtamIII@gmail.com>
2024-02-06 19:28:37 -0700

    Server booting, with 404

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
-const catRoutes = require('./catRoutes')
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
-router.use('/cats', catRoutes);
 
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
-// This is example code- does not correlate to this project. Will need to be changed
+// This is example code- does not correlate to this pet. Will need to be changed
 
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const name = document.querySelector('#pet-name').value.trim();
+  const needed_funding = document.querySelector('#pet-funding').value.trim();
+  const description = document.querySelector('#pet-desc').value.trim();
 
   if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+    const response = await fetch(`/api/pets`, {
       method: 'POST',
       body: JSON.stringify({ name, needed_funding, description }),
       headers: {
@@ -19,7 +19,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create pet');
     }
   }
 };
@@ -28,22 +28,22 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/pets/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete pet');
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-pet-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.pet-list')
   .addEventListener('click', delButtonHandler);

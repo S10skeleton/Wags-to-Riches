58877b20429892257939f0b887469a273612ca8d
Thomas Holtam <thomasjholtamIII@gmail.com>
2024-02-09 14:24:34 -0700

    Added buttons and function to sorting pets and split the routes into seperate files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const catRoutes = require('./catRoutes');
+const dogRoutes = require('./dogRoutes');
 
-router.use('/users', userRoutes);
+
+router.use('/users', userRoutes); 
+router.use('/cat', catRoutes); 
+router.use('/dog', dogRoutes);  
 
 module.exports = router;
+
 
 
